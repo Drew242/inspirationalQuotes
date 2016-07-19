7bbc790c79f208ef82e8c782ac5a019e4878cf6c
Drew Conly <atconly@gmail.com>
2016-07-19 12:44:51 -0600

    make responsive

--- main.js
+++ main.js
@@ -4,7 +4,41 @@ angular.module('quoteApp', []);
 angular.module('quoteApp')
   .controller('quoteCtrl', quoteCtrl);
 
-function quoteCtrl() {
+angular.module('quoteApp')
+  .factory('staticQuotes', staticQuotes)
+
+function staticQuotes() {
+
+  var quotes = [
+    {
+      quote: "Everyone has a plan 'till they get punched in the mouth",
+      author: "Mike Tyson"
+    },
+    {
+      quote: "I think you've confused me with someone who builds a dam",
+      author: "Beavers"
+    },
+    {
+      quote: "There's no 'I' in 'Denial'",
+      author: "People"
+    },
+    {
+      quote: "A lion doesn't concern itself with the opinion of sheep",
+      author: "George R.R. Martin"
+    },
+    {
+      quote: "He that lives upon hope will die fasting",
+      author: "Ben Franklin"
+    }
+  ]
+
+  return {
+    getQuotes: quotes
+  }
+}
+
+function quoteCtrl(staticQuotes) {
   var quote = this;
-  quote.banner = "Inspirational Quotes";
+  quote.banner  = "Inspirational Quotes";
+  quote.statics = staticQuotes.getQuotes;
 }
